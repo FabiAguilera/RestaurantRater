a63d7db19ef5d88c1ef2cc25f737cdb5ad0daafd
Fabiola Aguilera <fabiaguilera96@gmail.com>
2022-03-09 18:49:44 -0500

    added to create method in create.cshtml, added create index in restaurant controller

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -15,5 +15,25 @@ namespace RestaurantRater.Controllers
         {
             return View(_db.Restaurants.ToList());
         }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");   // if ModelState is not IsValid (false), then we return to the View.
+            }
+            return View(restaurant);    // If ModelState is IsValid (true), then we add and save changes and return a new method called RedirectToCTion("Index"); which takes the string name of another action (the Index method) and redirects the code to that action. Here, the return kicks us over to the Index action and takes us to the Index view.
+        }
     }
 }
